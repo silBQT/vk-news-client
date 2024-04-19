a09624771ba8b05807691315b745308380a2e301
Bakyt <aziatkg102@gmail.com>
2024-04-19 08:09:14 +0300

    feat(MainScreen.kt): add FAB, Snackbar (lesson 6)

--- app/src/main/java/com/silbqt/vknewsclient/ui/theme/MainScreen.kt
+++ app/src/main/java/com/silbqt/vknewsclient/ui/theme/MainScreen.kt
@@ -1,24 +1,62 @@
 package com.silbqt.vknewsclient.ui.theme
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
 @Preview
 @Composable
 fun MainScreen() {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val isFabVisible = remember { mutableStateOf(true) }
+
     Scaffold (
+        snackbarHost = {
+           SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            if (isFabVisible.value) {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        val action = snackbarHostState.showSnackbar(
+                            message = "This is a snackbar",
+                            actionLabel = "Hide FAB",
+                            duration = SnackbarDuration.Long
+                        )
+                        if (action == SnackbarResult.ActionPerformed) {
+                            isFabVisible.value = false
+                        }
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = null
+                    )
+                }
+            }
+
+        },
         bottomBar = {
             NavigationBar {
                 val selectedItemPosition = remember {
